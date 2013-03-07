06b643e60374e0e89e2c46682c6fdeedda0b666a
David Rajchenbach-Teller <dteller@mozilla.com>
2013-03-07 22:48:42 +0100

    Made parametric

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-var object = {};
+////////////// Setup test
 
-for (var i = 0; i < 1000000; ++i) {
-  object[":" + i] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+var NUMBER_OF_SAMPLES = 100;
+var LEAVES_IN_SAMPLE = 1000000;
+var BYTES_IN_LEAVE = 50;
+
+// Control test size with a url
+if (window.location.search.length > 1) {
+  (function() {
+    var args = window.location.search.substr(1).split("&");
+    var i;
+    for (i = 0; i < args.length; ++i) {
+      var arg = args[i];
+      if (arg.startsWith("number=")) {
+        NUMBER_OF_SAMPLES = parseInt(arg.substr("number=".length));
+      } else if (arg.startsWith("leaves=")) {
+        LEAVES_IN_SAMPLE = parseInt(arg.substr("leaves=".length));
+      } else if (arg.startsWith("bytes=")) {
+        BYTES_IN_LEAVE = parseInt(arg.substr("bytes=".length));
+      }
+    }
+  })();
+}
+
+
+var eltMainThread = document.getElementById("mainthread");
+var eltWorkerThread = document.getElementById("workerthread");
+var eltJSON = document.getElementById("json");
+
+// Initialize a huge object for serialization
+var hugeObject = {};
+var leaf = "";
+for (var i = 0; i < BYTES_IN_LEAVE; ++i) {
+  leaf += "x";
+}
+
+for (var i = 0; i < LEAVES_IN_SAMPLE; ++i) {
+  hugeObject[":" + i] = leaf;
 }
 
 var worker = new Worker("./worker.js");
@@ -11,15 +45,19 @@ worker.postMessage("");
 
 var deltas = [];
 
-for (i = 0; i < 1; ++i) {
-  window.setTimeout(function() {
-    console.log("Sending object");
-    var start = Date.now();
-    worker.postMessage(object);
-    var end = Date.now();
-    deltas.push(end - start);
-    console.log("Object sent", end - start);
-  });
+
+for (i = 0; i < NUMBER_OF_SAMPLES; ++i) {
+  var scope = (function(i) {
+    window.setTimeout(function() {
+      console.log("Sending hugeObject");
+      var start = Date.now();
+      worker.postMessage(hugeObject);
+      var end = Date.now();
+      deltas.push(end - start);
+      console.log("HugeObject sent", end - start);
+      eltMainThread.textContent = "Sent " + ( i + 1 ) + "/" + NUMBER_OF_SAMPLES;
+    });
+  })(i);
 }
 
 window.setTimeout(function() {
@@ -28,17 +66,19 @@ window.setTimeout(function() {
 });
 
 
+var receivedMessages = 0;
 worker.addEventListener("message", function onmessage(e) {
   var data = e.data;
-  if (!Array.isArray(data)) {
+  if (!("deltas" in data)) {
     // Ignore message
+    eltWorkerThread.textContent = "Received " + (++receivedMessages) + "/" + NUMBER_OF_SAMPLES;
     return;
   }
-  var eltMainThread = document.getElementById("mainthread");
-  eltMainThread.textContent = "Main thread serialization (ms): " + JSON.serialize(deltas);
-  var eltWorkerThread = document.getElementById("workerthread");
-  eltWorkerThread.textContent = "Worker thread serialization (ms): " + JSON.serialize(deltas);
+  eltMainThread.textContent = "Main thread serialization (ms): " + deltas.join(", ");
+  eltWorkerThread.textContent = "Worker thread serialization (ms): " + data.deltas.join(", ");
+  eltJSON.textContent = "JSON serialization (ms): " + data.serialize;
 
   console.log("Serialization cost", "(main thread)", deltas);
-  console.log("Serialization cost", "(worker thread)", data);
+  console.log("Serialization cost", "(worker thread)", data.deltas);
+  console.log("Serialization to JSON cost", "(worker thread)", data.serialize);
 });
--- worker.js
+++ worker.js
@@ -1,15 +1,28 @@
+var object = null;
 var deltas = [];
+
+var measureDuration = function measureDuration(f) {
+  var start = Date.now();
+  f();
+  var end = Date.now();
+  return end - start;
+};
+
 self.addEventListener("message", function onmessage(e) {
   var data = e.data;
   if (data == "") {
     return;
   }
   if (data == "end") {
-    self.postMessage(deltas);
+    // Measure the duration of JSON serialization, for comparison
+    var serialize = measureDuration(function() {
+      JSON.stringify(object);
+    });
+    self.postMessage({deltas: deltas, serialize: serialize});
     return;
   }
-  var start = Date.now();
-  self.postMessage(data);// Post message back to sender
-  var end = Date.now();
-  deltas.push(end - start);
+  object = data;
+  deltas.push(measureDuration(function() {
+    self.postMessage(data);// Post message back to sender
+  }));
 });
